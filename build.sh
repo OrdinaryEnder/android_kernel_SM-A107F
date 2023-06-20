0a6f3ac1342100693e83e587e3ffbe5593eef976
anuragpindoriya <anuragpindoriya@gmail.com>
2023-06-20 19:36:30 +0530

    make build script and fix open.c

---
+++ build.sh
@@ -0,0 +1,8 @@
+config=a10s_defconfig
+
+export PATH="/home/anurag/Downloads/toolchains/clang-r428724/bin:/home/anurag/Downloads/toolchains/aarch64-linux-android-4.9/bin:/home/anurag/Downloads/toolchains/arm-linux-androideabi-4.9/bin:$PATH"
+
+export ARCH=arm64
+export CROSS_COMPILE=aarch64-linux-android-
+export CROSS_COMPILE_ARM32=arm-linux-androideabi-
+export CLANG_TRIPLE=aarch64-linux-gnu-
